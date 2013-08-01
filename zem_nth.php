01fae9736d28dd3313063e546dcf9860938f757d
Jukka Svahn <web@rahinaa.biz>
2013-08-01 13:48:53 +0300

    Reduces indentation level.

--- zem_nth.php
+++ zem_nth.php
@@ -21,60 +21,60 @@
  * @return string User markup
  */
 
-    function zem_nth($atts, $thing = null)
+function zem_nth($atts, $thing = null)
+{
+    static $counter = array(), $range = array();
+
+    extract(lAtts(array(
+        'step'  => 2,
+        'of'    => PHP_INT_MAX,
+        'id'    => null,
+        'start' => 0,
+        'reset' => 0,
+    ), $atts));
+
+    if ($id === null)
     {
-        static $counter = array(), $range = array();
+        $id = json_encode(array($step, $thing, $of));
+    }
 
-        extract(lAtts(array(
-            'step'  => 2,
-            'of'    => PHP_INT_MAX,
-            'id'    => null,
-            'start' => 0,
-            'reset' => 0,
-        ), $atts));
+    if (!isset($range[$id]))
+    {
+        $range[$id] = array();
 
-        if ($id === null)
+        // Expands a list of "1, 2, 3-7, 8" into an array of integers.
+
+        foreach (do_list($step, ',') as $value)
         {
-            $id = json_encode(array($step, $thing, $of));
-        }
-
-        if (!isset($range[$id]))
-        {
-            $range[$id] = array();
-
-            // Expands a list of "1, 2, 3-7, 8" into an array of integers.
-
-            foreach (do_list($step, ',') as $value)
+            if (strpos($value, '-'))
             {
-                if (strpos($value, '-'))
-                {
-                    $value = do_list($value, '-');
-                    $range[$id] = array_merge($range[$id], range((int) $value[0], (int) $value[1]));
-                }
-                else
-                {
-                    $range[$id][] = (int) $value;
-                }
+                $value = do_list($value, '-');
+                $range[$id] = array_merge($range[$id], range((int) $value[0], (int) $value[1]));
+            }
+            else
+            {
+                $range[$id][] = (int) $value;
             }
         }
-
-        if (!isset($counter[$id]) || $reset)
-        {
-            $counter[$id] = (int) $start;
-        }
-
-        $counter[$id]++;
-
-        if ($thing === null)
-        {
-            $out = $counter[$id];
-        }
-        else
-        {
-            $out = parse(EvalElse($thing, in_array($counter[$id], $range[$id])));
-        }
-
-        $counter[$id] = $counter[$id] % $of;
-
-        return $out;
     }
+
+    if (!isset($counter[$id]) || $reset)
+    {
+        $counter[$id] = (int) $start;
+    }
+
+    $counter[$id]++;
+
+    if ($thing === null)
+    {
+        $out = $counter[$id];
+    }
+    else
+    {
+        $out = parse(EvalElse($thing, in_array($counter[$id], $range[$id])));
+    }
+
+    $counter[$id] = $counter[$id] % $of;
+
+    return $out;
+}
